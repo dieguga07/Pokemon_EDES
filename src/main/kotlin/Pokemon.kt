447a40051bb007c5bd0eed8d0d2013234b6cab94
Diego Gutierrez <dieguga04@gmail.com>
2023-03-25 14:59:59 +0100

    Arreglo nuevo

--- src/main/kotlin/Pokemon.kt
+++ src/main/kotlin/Pokemon.kt
@@ -1,6 +1,4 @@
 
-class Pokemon(nombre: String, lore: String, tipo:Tipo, fuerza:Int, vida:Int) {
-
 /**
  * # Comentarios sobre la clase [Pokemon]
  *
@@ -13,26 +11,27 @@ class Pokemon(nombre: String, lore: String, tipo:Tipo, fuerza:Int, vida:Int) {
 
 
     //variables que posee la clase
-     var nombre = nombre
-     var lore = lore
-     var fuerza = fuerza
-     var vida = vida
+    var nombre = nombre
+    var lore = lore
+    var fuerza = fuerza
+    var vida = vida
 
 // funcion para recibir un ataque de otro jugador
 
-     fun recibirAtaque(ataque_recibido: Ataque) {
+    fun recibirAtaque(ataque_recibido: Ataque) {
 
-         if ("duplicado" == "") {
-             var ataque = ataque_recibido.dano * 2
-             vida - ataque
-         }
-         if ("normal" == "") {
-             var ataque = ataque_recibido.dano * 1
-           vida - ataque
-         }
-         if ("mitad" == "") {
-             var ataque = ataque_recibido.dano * 0.5
-             vida - ataque
-         }
+        if ("duplicado" == "") {
+            var ataque = ataque_recibido.dano * 2
+            vida - ataque
+        }
+        if ("normal" == "") {
+            var ataque = ataque_recibido.dano * 1
+            vida - ataque
+        }
+        if ("mitad" == "") {
+            var ataque = ataque_recibido.dano * 0.5
+            vida - ataque
+        }
 
-     }
+    }
+}
