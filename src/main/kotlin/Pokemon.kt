b037b0dfec4657eaaecc967ab3894a45342c4923
Diego Gutierrez <dieguga04@gmail.com>
2023-03-25 14:56:53 +0100

    Nada

--- src/main/kotlin/Pokemon.kt
+++ src/main/kotlin/Pokemon.kt
@@ -1,4 +1,4 @@
- class Pokemon(nombre: String, lore: String, tipo:Tipo, fuerza:Int, vida:Int) {
+class Pokemon(nombre: String, lore: String, tipo:Tipo, fuerza:Int, vida:Int) {
 
      var nombre = nombre
      var lore = lore
